795eec080aef334403ea137e07f4dcd6ded950dc
Avery Maxwell <averymaxwell42@gmail.com>
2024-02-28 20:59:57 -0600

    Assign num-container and buttons to variables

---
+++ app.js
@@ -0,0 +1,4 @@
+const numberDisplay = document.querySelector('.num-container');
+const decreaseButton = document.querySelector('.decrease-button');
+const resetButton = document.querySelector('.reset-button');
+const increaseButton = document.querySelector('.increase-button');
