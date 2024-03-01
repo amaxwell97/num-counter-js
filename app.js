0420f7449c3258c8cfcedcf5e16055f7f5843951
Avery Maxwell <averymaxwell42@gmail.com>
2024-02-29 19:54:08 -0600

    Create setNumColor fn, add to end of other fns

--- app.js
+++ app.js
@@ -5,20 +5,33 @@ const increaseButton = document.querySelector('.increase');
 
 let currentNum = numberDisplay.innerText;
 
+const setNumColor = () => {
+    if (Number(currentNum) < 0 ) {
+        numberDisplay.style.color = '#D70000';
+    } else if (Number(currentNum) > 0){
+        numberDisplay.style.color = '#32CD32';
+    } else {
+        numberDisplay.style.color = '#000';
+    }
+}
+
 const increaseNumber = () => {
     ++currentNum;
     numberDisplay.innerText = currentNum.toString();
+    setNumColor();
 }
 increaseButton.addEventListener('click', increaseNumber);
 
 const decreaseNumber = () => {
     --currentNum;
     numberDisplay.innerText = currentNum.toString();
+    setNumColor();
 }
 decreaseButton.addEventListener('click', decreaseNumber);
 
 const resetNumber = () => {
     currentNum = 0;
     numberDisplay.innerText = currentNum.toString();
+    setNumColor();
 }
 resetButton.addEventListener('click', resetNumber);
