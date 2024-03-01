209d0622ec5d4faf14184620ecdd88ea82f54e53
Avery Maxwell <averymaxwell42@gmail.com>
2024-02-29 19:34:01 -0600

    Create resetNumber fn, set event listener

--- app.js
+++ app.js
@@ -9,12 +9,16 @@ const increaseNumber = () => {
     ++currentNum;
     numberDisplay.innerText = currentNum.toString();
 }
-
 increaseButton.addEventListener('click', increaseNumber);
 
 const decreaseNumber = () => {
     --currentNum;
     numberDisplay.innerText = currentNum.toString();
 }
-
 decreaseButton.addEventListener('click', decreaseNumber);
+
+const resetNumber = () => {
+    currentNum = 0;
+    numberDisplay.innerText = currentNum.toString();
+}
+resetButton.addEventListener('click', resetNumber);
